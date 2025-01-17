087880ca9909b17583d69df5f25c81f3b97558cc
Sergey-77 <46878504+Sergey-77@users.noreply.github.com>
2025-01-17 15:54:16 -0500

    Added quadratic polynomial to calculate temperature from ADC value

--- src/modules/tools/temperaturecontrol/pt100.cpp
+++ src/modules/tools/temperaturecontrol/pt100.cpp
@@ -12,11 +12,9 @@
 #define UNDEFINED -1
 
 // Manual http://smoothieware.org/3d-printer-guide#toc12
-// Spreadsheet with help to configure: http://smoothieware.org/local--files/3d-printer-guide/PT100%20E3D.ods
-
 
 #define pt100linear_checksum               CHECKSUM("pt100linear")        // if 1 then use slope/yintercept, if 0 use
-                                                                          // A, B, R0
+                                                                          // a, b, c  in quadratic polynomial
 #define thermistor_pin_checksum            CHECKSUM("thermistor_pin")
 #define ampmod1_pin_checksum               CHECKSUM("ampmod1_pin")        // UP! printer uses this to "energize" the RTD
 #define ampmod2_pin_checksum               CHECKSUM("ampmod2_pin")        // set as nc if you don't need to energize RTD
@@ -24,14 +22,9 @@
 #define yintercept_checksum                CHECKSUM("yintercept")         // SLOPE and Y-Intercept to have minimal error
                                                                           // in the region you use 
 
-#define pt100A_checksum                    CHECKSUM("pt100_a")            // A value
-#define pt100B_checksum                    CHECKSUM("pt100_b")            // B value
-#define pt100R0_checksum                   CHECKSUM("pt100_r0")           // R0 value
-#define pt100amptype_checksum              CHECKSUM("pt100_amptype")      // Amp type between RTD and ADC input
-                                                                          // 0: e3d pt100 amp http://wiki.e3d-online.com/wiki/E3D_PT100_Amplifier_Documentation
-#define pt100q1_checksum                   CHECKSUM("pt100_q1")           // Q1 for ADC to RES formula
-#define pt100q2_checksum                   CHECKSUM("pt100_q2")           // Q2 for ADC to RES formula
-#define pt100q3_checksum                   CHECKSUM("pt100_q3")           // Q3 for ADC to RES formula
+#define pt100_a_checksum                    CHECKSUM("pt100_a")            // a value
+#define pt100_b_checksum                    CHECKSUM("pt100_b")            // b value
+#define pt100_c_checksum                    CHECKSUM("pt100_c")            // c value
 
 PT100::PT100()
 {
@@ -40,10 +33,10 @@ PT100::PT100()
     this->use_ampmod2= false;
     this->min_temp= 999;
     this->max_temp= 0;
-    this->m = 0;             //use as slope or A 
-    this->b = 0;             //use as yintercept or B
-    this->r0= 0;             // R0 - temp of the RTD and 0C (100 for PT100)
-    this->use_linear= true;  // true: use linear, false: use fancy math
+    this->a = 0;             //use as slope or a 
+    this->b = 0;             //use as yintercept or b
+    this->c = 0;
+    this->use_linear= true;  // true: use linear, false: polynomial 
 }
 
 PT100::~PT100()
@@ -58,16 +51,12 @@ void PT100::UpdateConfig(uint16_t module_checksum, uint16_t name_checksum)
     this->use_linear = THEKERNEL->config->value(module_checksum, name_checksum,  pt100linear_checksum)->by_default(1)->as_number() > 0;
 
     if (this->use_linear){
-      this->m = THEKERNEL->config->value(module_checksum, name_checksum, slope_checksum      )->by_default(  0.0257604875F )->as_number(); // default value for UP! 
+      this->a = THEKERNEL->config->value(module_checksum, name_checksum, slope_checksum      )->by_default(  0.0257604875F )->as_number(); // default value for UP! 
       this->b = THEKERNEL->config->value(module_checksum, name_checksum, yintercept_checksum )->by_default(-18.54F         )->as_number(); // PT100 in the hotend
     }else{
-      this->m = THEKERNEL->config->value(module_checksum, name_checksum, pt100A_checksum )->by_default( 3.9083E-3F )->as_number(); // ALFA (for PT100 ITS-90 it is 3.9083E-3 )
-      this->b = THEKERNEL->config->value(module_checksum, name_checksum, pt100B_checksum )->by_default( 5.775E-7F  )->as_number(); // BETA for the RTD (for PT100 ITS-90 it is 5.775E-7 )
-      this->r0= THEKERNEL->config->value(module_checksum, name_checksum, pt100R0_checksum)->by_default( 100        )->as_number(); // R0 - resistance on 0C (for PT100 it is 100)
-      this->amptype= THEKERNEL->config->value(module_checksum, name_checksum, pt100amptype_checksum)->by_default(0 )->as_number(); // Amp type
-      this->q1= THEKERNEL->config->value(module_checksum, name_checksum, pt100q1_checksum )->by_default( 805200    )->as_number(); // Q1 for e3d
-      this->q2= THEKERNEL->config->value(module_checksum, name_checksum, pt100q2_checksum )->by_default( 45409000  )->as_number(); // Q2 for e3d
-      this->q3= THEKERNEL->config->value(module_checksum, name_checksum, pt100q3_checksum )->by_default( 183       )->as_number(); // Q3 for e3d
+      this->a = THEKERNEL->config->value(module_checksum, name_checksum, pt100_a_checksum )->by_default( 0.000000174674754F )->as_number(); // default values
+      this->b = THEKERNEL->config->value(module_checksum, name_checksum, pt100_b_checksum )->by_default( 0.022383F          )->as_number(); // for Cetus mk3
+      this->c = THEKERNEL->config->value(module_checksum, name_checksum, pt100_c_checksum )->by_default( -4.00648F          )->as_number(); // 
     }
     THEKERNEL->adc->enable_pin(&thermistor_pin);
 
@@ -95,7 +84,7 @@ float PT100::get_temperature()
     return t;
 }
 
-void PT100::get_raw() //no clue what/where/when is this used for
+void PT100::get_raw()
 {
     if(this->bad_config) {
        THEKERNEL->streams->printf("WARNING: The config is bad for this temperature sensor\n");
@@ -120,43 +109,10 @@ float PT100::adc_value_to_temperature(uint32_t adc_value)
         // use the simple linear equation allowing slope and y-intersect to 
         // be configurable so user can tune for particular RTD and tune for 
         // minimal error on the required span
-        t = adc_value * this->m + this->b;
+        t = adc_value * this->a + this->b;
     } else {
-        // proper RTD calculation should be like this
-        // but depending on how the RTD is connected, is there 
-        // some amp after it, or any other circuit.. 
-        // I'm not using it
-        //
-        //float R0 = 100;     //R0 - temp of the RTD at zero
-        //float res;  //resistance of the RTD that we want temp from
-        //float alpha = 3.9083E-3; // A for the RTD (for PT100 ITS-90 it is 3.9083E-3 )
-        //float betha = 5.775E-7;  // B for the RTD (for PT100 ITS-90 it is 5.775E-7 )
-        // WRONG -> //t = (-R0 * alpha + sqrtf(R0 * R0 * + alpha * alpha - 4 * R0 * betha * (R0 - res))) / (2 * R0 * - betha);
-        // wolfram rulez :D
-        // https://www.wolframalpha.com/input/?i=X+%3D+R+*+(1+%2B+A+*+T+%2B+B+*+T%5E2+)+solve+for+T
-        // T = (SQRT(A^2*R0 +4*B*(Rt – R0)) - A*SQRT(R0)) / (2*B*SQRT(R0))
-
-        // there is a problem on how to get RES from ADC, especially if there is some AMP in front of ADC pin
-        // and usually there is one. I'm solving this by "hardcoding amp type parameters in code"
-        // as I can't think how to do it smarter
-        switch (this->amptype){
-	case 0: // e3d pt100 amp
-           t =  this->q1 * adc_value / (this->q2 - this->q3 * adc_value); // Calculate resistance from ADC value
-           break;
-        default: // TODO: add more amp types
-           t = infinityf();
-        }
-        t = ( 
-              sqrtf(
-                this->m * this->m * this->r0 + 4 * this->b * (t - this->r0)
-              ) 
-              - 
-              this->m * sqrtf(this->r0)
-            ) 
-            / 
-            (
-              2 * this->b * sqrtf(this->r0)
-            );
+    	// use quadratic polynomial to calculate temperature from ADC value
+    	 t = this->a * adc_value * adc_value + this->b * adc_value + this->c;
     }
     return t;
 }
@@ -176,5 +132,3 @@ int PT100::new_thermistor_reading()
 
     return t;
 }
-
-
